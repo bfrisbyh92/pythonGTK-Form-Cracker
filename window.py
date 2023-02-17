0059ed0451ece0efe84a8b833e2a951261cfb21f
bfrisbyh92 <bfrisbyh92@gmail.com>
2023-02-16 19:55:09 -0500

    .

--- window.py
+++ window.py
@@ -1,43 +1,61 @@
-import subprocess
-import gi
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import gi
+import subprocess
+
+gi.require_version('Gtk', '3.0')
+
 
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="GTK App")
 
-        # Create a vertical box layout
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
 
-        # Create a button for each command to execute
+        # Create a horizontal box container for the buttons
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        vbox.pack_start(hbox, False, False, 0)
+
+        # Create the buttons
         button1 = Gtk.Button.new_with_label("ls")
-        button2 = Gtk.Button.new_with_label("ps aux")
-        button3 = Gtk.Button.new_with_label("whoami")
+        button2 = Gtk.Button.new_with_label("ps")
+        button3 = Gtk.Button.new_with_label("pwd")
 
-        # Create a text view to display the command output
+        # Add the buttons to the horizontal box container
+        hbox.pack_start(button1, True, True, 0)
+        hbox.pack_start(button2, True, True, 0)
+        hbox.pack_start(button3, True, True, 0)
+
+        # Create a textview for displaying the output
         self.textview = Gtk.TextView()
         vbox.pack_start(self.textview, True, True, 0)
 
-        # Connect button clicks to commands
-        button1.connect("clicked", self.run_command, "ls")
-        button2.connect("clicked", self.run_command, "ps aux")
-        button3.connect("clicked", self.run_command, "whoami")
+        # Connect the button signals to their handlers
+        button1.connect("clicked", self.on_button1_clicked)
+        button2.connect("clicked", self.on_button2_clicked)
+        button3.connect("clicked", self.on_button3_clicked)
 
-        # Add buttons to the layout
-        vbox.pack_start(button1, True, True, 0)
-        vbox.pack_start(button2, True, True, 0)
-        vbox.pack_start(button3, True, True, 0)
+        # Add the vertical box container to the window
+        self.add(vbox)
 
-    def run_command(self, widget, command):
-        # Run the command as a subprocess and capture its output
-        result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def on_button1_clicked(self, button):
+        command = ["ls"]
+        output = subprocess.check_output(command).decode()
+        self.update_textview(output)
 
-        # Display the output in the text view
-        output = result.stdout.decode() + result.stderr.decode()
+    def on_button2_clicked(self, button):
+        command = ["ps", "aux"]
+        output = subprocess.check_output(command).decode()
+        self.update_textview(output)
+
+    def on_button3_clicked(self, button):
+        command = ["pwd"]
+        output = subprocess.check_output(command).decode()
+        self.update_textview(output)
+
+    def update_textview(self, text):
         buffer = self.textview.get_buffer()
-        buffer.set_text(output)
+        buffer.set_text(text)
+
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
